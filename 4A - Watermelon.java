d879b282ffb263044dd83e2eb8741e0efe8e6677
EyadTamer999 <103904839+EyadTamer999@users.noreply.github.com>
2022-05-09 22:00:47 +0200

    Update 4A - Watermelon.java

--- 4A - Watermelon.java
+++ 4A - Watermelon.java
@@ -1,18 +1,25 @@
+import java.util.Scanner;
 
-public class Solution {
+public class Solution_2 {
 
 	public static void main(String[] args) {
 		
-		//they need to tell wheather it is even or not, so basically check if it can be an even number or not...
+		//they need to tell whether it is even or not, so basically check if it can be an even number or not...
 		
-		System.out.println(waterMelon(8));
+		Scanner sc = new Scanner(System.in);
+		int kilo = sc.nextInt();
+		
+		if(kilo <= 1 || kilo <= 100)
+			System.out.println(waterMelon(kilo));
+		else
+			System.out.println("NO");
 	}
 
 	public static String waterMelon(int i) {
 		if(i % 2 == 0)
-			return "Yes";
+			return "YES";
 		else
-			return "No";
+			return "NO";
 	}
 
 	
