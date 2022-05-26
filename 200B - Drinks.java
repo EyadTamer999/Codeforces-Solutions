be9351aa28666c3d1c3b31e9302255de2867a98a
EyadTamer999 <103904839+EyadTamer999@users.noreply.github.com>
2022-05-27 00:57:55 +0200

    Update 200B - Drinks.java

--- 200B - Drinks.java
+++ 200B - Drinks.java
@@ -2,23 +2,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-
+ 
 public class Solution_2 {
-
-	public static void main(String[] args) {
+ 
+	public static void main(String[] args) throws IOException {
 	//200B Drinks
 	// divide each drink % over total number of drinks to get res
-	Scanner sc = new Scanner(System.in);
-	double n = sc.nextInt();
+	
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String lines = null;
-	try {
-		lines = br.readLine();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}   
-	String[] d = lines.trim().split(" ");
+    String tmp = br.readLine();
+    String m[] = tmp.split(" ");
+    double n = Integer.parseInt(m[0]);
+	String lines = br.readLine(); 
+	String[] d = lines.split(" ");
 	double res = 0;
 	for(int i = 0; i < d.length; i++) {
 		if(d[i] != null)
